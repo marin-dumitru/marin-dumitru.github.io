d882bea95a45336d37d43695d91ef1c7a91bff87
Marin Dumitru-Daniel <combidan@gmail.com>
2020-04-04 06:47:13 +0300

    Site TW

---
+++ JS/footer.js
@@ -0,0 +1,32 @@
+document.write('\
+\
+      <footer class="main-footer">\
+    <div class="flex-container">\
+      <section class="item-1">\
+       <h1>Despre <a href="index.html"><span class="title-color">VFIT</span> GYM</a></h1>\
+        <p> VFIT Gym a fost infiintata in anul 2016 cu scopul de a oferi noi oportunitati oamenilor. Programul NON STOP, atmosfera familiala cat si aparaturile fac din VFIT Gym o sala primitoare, pregatita sa faca fata tuturor cerintelor dumneavoastra. </p>\
+        <p>VFIT Gym va incerca sa deschida peste 20 de locatii cu program NON STOP pana la finalul anului 2020.</p>\
+        <div class="social-bar">\
+          <a href="#"><i class="social-icon fab fa-facebook-f"></i></a>\
+          <a href="#"><i class="social-icon fab fa-twitter"></i></a>\
+          <a href="#"><i class="social-icon fab fa-instagram"></i></a>\
+          <a href="#"><i class="social-icon fab fa-pinterest-p"></i></a>\
+          <a href="#"><i class="social-icon fab fa-google-plus-g"></i></a>\
+          <a href="#"><i class="social-icon fas fa-rss"></i></a>\
+        </div>\
+        <!-- End of Social Bar -->\
+      </section>\
+      <!-- End of Item-1 -->\
+      <section class="item-2">\
+        <p class="hello">Contact:</p>\
+        <p class="contact-info"><i class="contact-icon fas fa-map-marker-alt"></i><b>Adresa:</b> Bd. Termona, nr.320</p>\
+        <p class="contact-info"><i class="contact-icon fas fa-envelope"></i><b>Email:</b> Vfit@fitness.com</p>\
+        <p class="contact-info"><i class="contact-icon fas fa-phone"></i><b>Telefon:</b> (+40) 732 239 754</p>\
+      </section>\
+      <!-- End of Item-2 -->\
+    </div>\
+    <!-- End of container -->\
+    <a href="#top" class="back-to-top"><i class="fas fa-angle-up"></i></a>\
+  </footer>\
+\
+');
---
+++ JS/header.js
@@ -0,0 +1,25 @@
+document.write('\
+\
+    <header class="main-header">\
+    <nav>\
+      <img src="assets/img/logo.jpg" alt="404 logo not found">\
+      <h1 class="fade-in" data-wow-delay=".5s"><a href="index.html"><span class="title-color">Vfit</span> Gym</a></h1>\
+      <ul>\
+        <li class="fade-in wow" id="delay-1" data-wow-delay=".5s"><a href="index.html">Acasa</a></li>\
+        <li class="fade-in wow" id="delay-2" data-wow-delay=".5s"><a href="Prezentare.html">Prezentare</a></li>\
+        <li class="nav-end fade-in wow" id="delay-3" data-wow-delay=".5s"><a href="Servicii.html">Servicii</a></li>\
+        <li class="nav-end fade-in wow dd" id="delay-3" data-wow-delay=".5s">\
+          Produse\
+        <ul class="ddcontent">\
+                <li><a href="produse.html#proteine">Proteine</a></li>\
+                <li><a href="produse.html#vitamine">Vitamine</a></li>\
+                <li><a href="produse.html#creatina">Creatina</a></li>\
+              </ul>\
+        </li>      \
+      </ul>\
+    </nav>\
+    \
+    \
+  </header>\
+\
+');
